bcfd39ac4f3cfcff707edb1faef3550404e94449
David Jørgensen <david@pnd.se>
2015-09-22 12:47:32 +0200

    A commit

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  var fragment = req.query._escaped_fragment_;
+
+  // If there is no fragment in the query params
+  // then we're not serving a crawler
+  if (!fragment) return next();
+
+  // If the fragment is empty, serve the
+  // index page
+  if (fragment === "" || fragment === "/")
+    fragment = "/index.html";
+
+  // If fragment does not start with '/'
+  // prepend it to our fragment
+  if (fragment.charAt(0) !== "/")
+    fragment = '/' + fragment;
+
+  // If fragment does not end with '.html'
+  // append it to the fragment
+  if (fragment.indexOf('.html') == -1)
+    fragment += ".html";
+
+  // Serve the static html snapshot
+  try {
+    var file = __dirname + "/snapshots" + fragment;
+    res.sendfile(file);
+  } catch (err) {
+    res.send(404);
+  }
+});
+
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/partials', express.static(__dirname + '/partials'));
